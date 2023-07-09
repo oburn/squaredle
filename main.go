ddf61ac3b6b4ece350a81e0f2248c62d816414bd
Oliver Burn <oliver@puppycrawl.com>
2023-07-10 00:22:44 +1000

    change formatting

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -10,6 +11,7 @@ func main() {
 	maxLen := flag.Int("max", 5, "Maximum word length")
 	dictFile := flag.String("dict", "/usr/share/dict/words", "Dictionary file")
 	flag.Parse()
+	flag.Args()
 
 	fmt.Printf("Loading dictionary from %s, min len %d, max len %d\n", *dictFile, *minLen, *maxLen)
 	dict := NewDict(*dictFile, *minLen, *maxLen)
@@ -25,7 +27,21 @@ func main() {
 		searcher: dict.Search,
 	}
 	paths := grid.solve()
+	// for _, path := range paths {
+	// 	fmt.Printf("solution: %s -> %v\n", path.word, path.steps)
+	// }
+
+	wordToPath := make(map[string]Path)
 	for _, path := range paths {
-		fmt.Printf("solution: %s -> %v\n", path.word, path.steps)
+		wordToPath[path.word] = path
+	}
+	var words []string
+	for k := range wordToPath {
+		words = append(words, k)
+	}
+	sort.Strings(words)
+
+	for _, word := range words {
+		fmt.Printf("%s\t-> %v\n", word, wordToPath[word].steps)
 	}
 }
