19d774f3c66a2a73e92337668c486a430182fbd3
Oliver Burn <oburn@atlassian.com>
2023-07-11 12:37:08 +1000

    now solve my parsing in arguments

--- main.go
+++ main.go
@@ -8,22 +8,22 @@ import (
 
 func main() {
 	minLen := flag.Int("min", 4, "Minimum word length")
-	maxLen := flag.Int("max", 5, "Maximum word length")
+	maxLen := flag.Int("max", 10, "Maximum word length")
 	dictFile := flag.String("dict", "/usr/share/dict/words", "Dictionary file")
 	flag.Parse()
-	flag.Args()
+	if len(flag.Args()) < 4 {
+		fmt.Printf("Error: not enough rows\n")
+		fmt.Printf("Usage: %s [options] row...\n", flag.Arg(0))
+		flag.PrintDefaults()
+		return
+	}
 
 	fmt.Printf("Loading dictionary from %s, min len %d, max len %d\n", *dictFile, *minLen, *maxLen)
 	dict := NewDict(*dictFile, *minLen, *maxLen)
 	fmt.Printf("Loaded %d words\n", len(dict.words))
 
 	grid := Grid{
-		rows: []string{
-			"umut",
-			"rcbu",
-			"noal",
-			"irte",
-		},
+		rows:     flag.Args(),
 		searcher: dict.Search,
 	}
 	paths := grid.solve()
